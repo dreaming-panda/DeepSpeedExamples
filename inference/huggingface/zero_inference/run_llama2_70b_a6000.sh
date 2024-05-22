9d135414b629edc34db3b46c1186c127036f5fe3
dreaming-panda <64370186+dreaming-panda@users.noreply.github.com>
2024-05-22 03:44:49 -0400

    Update run_llama2_70b_a6000.sh

--- inference/huggingface/zero_inference/run_llama2_70b_a6000.sh
+++ inference/huggingface/zero_inference/run_llama2_70b_a6000.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 export USE_TF=0 
 BASE_LOG_DIR=~/experiments/zero_inference/
-MODEL_NAME="Llama-2-70b-hf"
+MODEL_NAME="Llama-2-70b-chat-hf"
 FULL_MODEL_NAME="meta-llama/${MODEL_NAME}"
 
 
 BSZ=1
 LOG_DIR=$BASE_LOG_DIR/${MODEL_NAME}_bs${BSZ}
 mkdir -p  $LOG_DIR
-deepspeed --num_gpus 1 run_model.py --model ${FULL_MODEL_NAME} --batch-size ${BSZ} --cpu-offload --gen-len 32 --pin-memory 1 &> $LOG_DIR/ds_${MODEL_NAME}_bs${BSZ}_cpu_pin.txt 
+deepspeed --num_gpus 1 run_model.py --model ${FULL_MODEL_NAME} --batch-size ${BSZ} --cpu-offload --gen-len 32 --pin-memory 1 
 
